5d6b3d2092e754d815f99b3a64d9a28bafbc8497
MuratYasar <yasar_murat@hotmail.com>
2022-12-29 23:03:28 +0300

    ILoggerManager added.

---
+++ OracleConnection.Core/Log/ILoggerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleConnection.Core.Log
+{
+    public interface ILoggerManager
+    {
+        void LogTrace(string message,   [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogDebug(string message,   [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogInfo(string message,    [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogSuccess(string message, [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogWarn(string message,    [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogError(string message,   [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+        void LogFatal(string message,   [CallerFilePath] string filePath = "",  [CallerMemberName] string memberName = "",  [CallerLineNumber] int lineNumber = 0);
+    }
+}
